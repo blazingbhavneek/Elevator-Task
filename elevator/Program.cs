10da88fabb483d2b1db1a7e2411e808cf1cf2f40
Bhavneek Singh <bhavneek2002@gmail.com>
2025-01-16 10:36:30 +0900

    easy logic done

--- elevator/Program.cs
+++ elevator/Program.cs
@@ -24,19 +24,19 @@ namespace Elevator
         public Building(int numFloors)
         {
             this.numFloors = numFloors;
-            this.floors = new Floor[numFloors + 1];
-            this.elevator = new Elevator();
+            floors = new Floor[numFloors + 1];
+            elevator = new Elevator();
             for (int i = 1; i <= numFloors; i++)
             {
                 this.floors[i] = new Floor();
             }
         }
 
-        public void handleKeyPress(int x)
+        public void HandleKeyPress(int x)
         {
-            this.floors[x].isTarget = true;
+            floors[x].isTarget = true;
         }
-        public void next()
+        public void Next()
         {
 
 
@@ -75,10 +75,6 @@ namespace Elevator
             }
             else
             {
-                if (floors[elevator.currentFloor].down)
-                {
-                    floors[elevator.currentFloor].down = false;
-                }
 
                 if (floors[elevator.currentFloor].isTarget)
                 {
@@ -107,11 +103,11 @@ namespace Elevator
         {
             Building building = new Building(10);  // Example with 5 floors
 
-            Timer timer = new Timer(_ => building.next(), null, 0, 1000);  // Run every 1 second
+            Timer timer = new Timer(_ => building.Next(), null, 0, 1000);  // Run every 1 second
 
             Console.WriteLine("Press 'q' to quit.");
 
-            int storedKey = 0
+            int storedKey = 0;
             while (true)
             {
                 var keyInfo = Console.ReadKey(intercept: true);  // Capture key press
@@ -123,7 +119,7 @@ namespace Elevator
                 {
                     // Get the number corresponding to the pressed key (1-9)
                     storedKey = keyInfo.Key - ConsoleKey.D1 + 1;
-                    building.handleKeyPress(storedKey);  // Call the handleKeyPress method to handle the key
+                    building.HandleKeyPress(storedKey);  // Call the handleKeyPress method to handle the key
                 }
             }
 
